42a6e7080dbdb16663027eccebecd41e0e16e4aa
dkakolyris <d.kakolyris@yahoo.gr>
2020-12-24 00:39:31 +0200

    a small dir app for tiffs

---
+++ Form1.Designer.vb
@@ -0,0 +1,113 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class TheForm
+	Inherits System.Windows.Forms.Form
+
+	'Form overrides dispose to clean up the component list.
+	<System.Diagnostics.DebuggerNonUserCode()> _
+	Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+		Try
+			If disposing AndAlso components IsNot Nothing Then
+				components.Dispose()
+			End If
+		Finally
+			MyBase.Dispose(disposing)
+		End Try
+	End Sub
+
+	'Required by the Windows Form Designer
+	Private components As System.ComponentModel.IContainer
+
+	'NOTE: The following procedure is required by the Windows Form Designer
+	'It can be modified using the Windows Form Designer.  
+	'Do not modify it using the code editor.
+	<System.Diagnostics.DebuggerStepThrough()> _
+	Private Sub InitializeComponent()
+		Me.SaveFileDialog1 = New System.Windows.Forms.SaveFileDialog()
+		Me.LoadButton = New System.Windows.Forms.Button()
+		Me.Label2 = New System.Windows.Forms.Label()
+		Me.FolderBrowserDialog1 = New System.Windows.Forms.FolderBrowserDialog()
+		Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+		Me.DontCheckBox = New System.Windows.Forms.CheckBox()
+		Me.UseBackSlashBox = New System.Windows.Forms.CheckBox()
+		Me.GroupBox1.SuspendLayout()
+		Me.SuspendLayout()
+		'
+		'SaveFileDialog1
+		'
+		Me.SaveFileDialog1.FileName = "output.csv"
+		Me.SaveFileDialog1.Filter = "CSV Files (*.csv)|*.csv|All files|*.*"
+		'
+		'LoadButton
+		'
+		Me.LoadButton.Location = New System.Drawing.Point(398, 2)
+		Me.LoadButton.Name = "LoadButton"
+		Me.LoadButton.Size = New System.Drawing.Size(75, 63)
+		Me.LoadButton.TabIndex = 1
+		Me.LoadButton.Text = "Load..."
+		Me.LoadButton.UseVisualStyleBackColor = True
+		'
+		'Label2
+		'
+		Me.Label2.Location = New System.Drawing.Point(12, 2)
+		Me.Label2.Name = "Label2"
+		Me.Label2.Size = New System.Drawing.Size(380, 63)
+		Me.Label2.TabIndex = 3
+		Me.Label2.Text = "Not run yet."
+		'
+		'GroupBox1
+		'
+		Me.GroupBox1.Controls.Add(Me.DontCheckBox)
+		Me.GroupBox1.Controls.Add(Me.UseBackSlashBox)
+		Me.GroupBox1.Location = New System.Drawing.Point(12, 68)
+		Me.GroupBox1.Name = "GroupBox1"
+		Me.GroupBox1.Size = New System.Drawing.Size(454, 112)
+		Me.GroupBox1.TabIndex = 4
+		Me.GroupBox1.TabStop = False
+		Me.GroupBox1.Text = "Options"
+		'
+		'DontCheckBox
+		'
+		Me.DontCheckBox.AutoSize = True
+		Me.DontCheckBox.Enabled = False
+		Me.DontCheckBox.Location = New System.Drawing.Point(6, 42)
+		Me.DontCheckBox.Name = "DontCheckBox"
+		Me.DontCheckBox.Size = New System.Drawing.Size(245, 17)
+		Me.DontCheckBox.TabIndex = 0
+		Me.DontCheckBox.Text = "Do not check again files found in dir output file"
+		Me.DontCheckBox.UseVisualStyleBackColor = True
+		'
+		'UseBackSlashBox
+		'
+		Me.UseBackSlashBox.AutoSize = True
+		Me.UseBackSlashBox.Enabled = False
+		Me.UseBackSlashBox.Location = New System.Drawing.Point(6, 19)
+		Me.UseBackSlashBox.Name = "UseBackSlashBox"
+		Me.UseBackSlashBox.Size = New System.Drawing.Size(288, 17)
+		Me.UseBackSlashBox.TabIndex = 0
+		Me.UseBackSlashBox.Text = "Use backslash for <fullpath> output (mysql compatibility)"
+		Me.UseBackSlashBox.UseVisualStyleBackColor = True
+		'
+		'TheForm
+		'
+		Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+		Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+		Me.ClientSize = New System.Drawing.Size(478, 185)
+		Me.Controls.Add(Me.GroupBox1)
+		Me.Controls.Add(Me.Label2)
+		Me.Controls.Add(Me.LoadButton)
+		Me.MaximizeBox = False
+		Me.Name = "TheForm"
+		Me.Text = "DirFolder"
+		Me.GroupBox1.ResumeLayout(False)
+		Me.GroupBox1.PerformLayout()
+		Me.ResumeLayout(False)
+
+	End Sub
+	Friend WithEvents SaveFileDialog1 As SaveFileDialog
+	Friend WithEvents LoadButton As Button
+	Friend WithEvents Label2 As Label
+	Friend WithEvents FolderBrowserDialog1 As FolderBrowserDialog
+	Friend WithEvents GroupBox1 As GroupBox
+	Friend WithEvents DontCheckBox As CheckBox
+	Friend WithEvents UseBackSlashBox As CheckBox
+End Class
---
+++ Form1.vb
@@ -0,0 +1,62 @@
+﻿Imports System.Drawing.Imaging
+Imports System.IO
+Imports System.Text
+
+Public Class TheForm
+	Private Sub LoadButton_Click(sender As Object, e As EventArgs) Handles LoadButton.Click
+		Const C As String = ";"
+		Dim dir As String, save As String
+		Dim append As Boolean
+		With FolderBrowserDialog1
+			.RootFolder = Environment.SpecialFolder.Desktop
+			If .ShowDialog(Me) <> DialogResult.OK Then Return
+			dir = .SelectedPath
+		End With
+		With SaveFileDialog1
+			.InitialDirectory = My.Computer.FileSystem.SpecialDirectories.Desktop
+			If .ShowDialog(Me) <> DialogResult.OK Then Return
+			save = .FileName
+			If (New FileInfo(save)).Exists Then
+				Select Case MsgBox("Append to existing file?", vbYesNoCancel Or MsgBoxStyle.Exclamation, "Append")
+					Case MsgBoxResult.Yes
+						append = True
+					Case MsgBoxResult.No
+						append = False
+					Case MsgBoxResult.Cancel
+						Return
+				End Select
+			End If
+		End With
+		If Not append Then IO.File.Delete(save)
+		Label2.Text = "Extracting directory info for " & dir & "..."
+		Application.DoEvents()
+		Dim files As FileInfo() = (New IO.DirectoryInfo(dir)).GetFiles(searchPattern:="*", searchOption:=SearchOption.AllDirectories)
+		Dim cur_frame_count As Integer
+		Using output As New StreamWriter(save, append, Encoding.UTF8)
+			output.NewLine = vbCrLf
+			If Not append Then output.WriteLine("name;path;size;mod_date;creation_date;image_file_pages")
+			Dim f As Integer = -1
+			Do
+				Try
+					For f = f + 1 To files.Count - 1
+						With files(f)
+							Using img As Image = Image.FromFile(.FullName)
+								cur_frame_count = img.GetFrameCount(FrameDimension.Page)
+							End Using
+							output.WriteLine(Replace(.Name, .Extension, "") & C & .FullName & C & .Length & C & .LastWriteTime & C & .CreationTime & C & cur_frame_count)
+							Label2.Text = f + 1 & "/" & files.Count & vbCrLf & "Saving info for " & .FullName & "..."
+							Application.DoEvents()
+						End With 'files(f)
+					Next f
+				Catch
+					With files(f)
+						output.WriteLine(Replace(.Name, .Extension, "") & C & .FullName & C & .Length & C & .LastWriteTime & C & .CreationTime & C & 0)
+						Label2.Text = f + 1 & "/" & files.Count & vbCrLf & "Saving info for " & .FullName & "..."
+						Application.DoEvents()
+					End With 'files(f)
+				End Try
+			Loop Until f >= files.Count - 1
+		End Using 'output
+		Label2.Text = "Done (" & files.Count & " files found)."
+	End Sub
+End Class
